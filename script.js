3ce28d752f69d2f8438a894aa2201bc25df2bf89
nandhakumarpv <163122818+nandhakumarpv@users.noreply.github.com>
2024-11-30 06:00:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Redirect after 2 minutes (120 seconds)  
+setTimeout(() => {  
+    window.location.href = "https://stg.jira.evolent.com/servicedesk/customer/user/login?nokerberos&destination=portals";  
+}, 120000); // 120000 milliseconds = 2 minutes
